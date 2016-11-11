2c7b6189a3881a8d8a70af4944b134f523df1bc6
Maria McLaughlin <mariamclaughlin@college.harvard.edu>
2016-11-11 12:02:21 -0500

    added data to the timeline and created temporary svg for vis

--- js/timeline.js
+++ js/timeline.js
@@ -10,6 +10,23 @@ Timeline = function(_parentElement, _data){
 Timeline.prototype.initVis = function() {
 	var vis = this;
 
+	vis.margin = { top: 40, right: 0, bottom: 60, left: 60 };
+
+	vis.width = $("#" + vis.parentElement).width() - vis.margin.left - vis.margin.right,
+		vis.height = 300 - vis.margin.top - vis.margin.bottom;
+
+	// SVG drawing area
+	vis.svg = d3.select("#" + vis.parentElement).append("svg")
+		.attr("width", vis.width + vis.margin.left + vis.margin.right)
+		.attr("height", vis.height + vis.margin.top + vis.margin.bottom)
+		.append("g")
+		.attr("transform", "translate(" + vis.margin.left + "," + vis.margin.top + ")");
+
+	var minMaxX = d3.extent(vis.data.map(function(d){ return d.dateend; }));
+
+	vis.svg.append("text")
+		.text("minimum and maximum dates: " + minMaxX);
+
 	// TO DO
 
 	vis.wrangleData();
@@ -17,6 +34,8 @@ Timeline.prototype.initVis = function() {
 
 Timeline.prototype.wrangleData = function() {
 	var vis = this;
+
+	console.log(vis.data);
 
 	// TO DO
 
