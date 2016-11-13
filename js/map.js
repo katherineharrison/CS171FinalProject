ccfd03696905ef205538cdb39b801232b2a766f9
Maria McLaughlin <mariamclaughlin@college.harvard.edu>
2016-11-13 16:14:23 -0500

    world map appears

--- js/map.js
+++ js/map.js
@@ -11,6 +11,35 @@ Map = function(_parentElement, _data){
 Map.prototype.initVis = function() {
 	var vis = this;
 
+	var width = 900,
+		height = 600;
+
+	var svg = d3.select("#" + vis.parentElement).append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	var projection = d3.geo.mercator()
+		.scale(250)
+		.translate([width / 1.4, height / 2])
+		.precision(.1);
+
+	var path = d3.geo.path()
+		.projection(projection);
+
+	// Load shapes of U.S. counties (TopoJSON)
+	d3.json("data/world110-m.json", function(error, data) {
+
+		// Convert TopoJSON to GeoJSON (target object = 'states')
+		var countries = topojson.feature(data, data.objects.countries).features;
+
+		// Render the U.S. by using the path generator
+		svg.selectAll(".country")
+			.data(countries)
+			.enter().append("path")
+			.attr("class", "country")
+			.attr("d", path);
+	});
+
 	console.log(vis.data);
 	// TO DO
 
